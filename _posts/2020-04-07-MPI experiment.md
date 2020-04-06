0b4cba5a374c80fc6c207ac3495a5b3166f7812e
magictomagic <magictomagic@z1172765646@gmail.com>
2020-04-07 01:06:44 +0800

    modified:   _posts/2020-04-07-MPI experiment.md 	new file:   files/SendRecv.c 	new file:   files/ini-sendRecv.c 	new file:   img/Snipaste_2020-04-06_20-01-33.png 	new file:   img/Snipaste_2020-04-07_00-53-02.png 	new file:   img/Snipaste_2020-04-07_01-05-23.png

--- _posts/2020-04-07-MPI experiment.md
+++ _posts/2020-04-07-MPI experiment.md
@@ -4,16 +4,27 @@
 
 代码见[附件][1]
 ## 并行版本
+#### 最原始的方法
+思路：通过将数据量除以进程个数，将数据分为进程数个块,主进程对矩阵赋初值，通过 MPI_Send 将矩阵广播到各进程, MPI_Recv 接收数据，计算结果后，再发送给主进程。
+实现：见[附件][5]
+测试：见下图
+![](/img/Snipaste_2020-04-07_00-53-02.png)
 #### 利用 MPI_Scatter 和 MPI_Gather
 思路：先将矩阵 A 和 C 按行分为 np 块，进程号为 id 的进程读取 A 的第 id 个分块和 B；进程号为 id 的进程求解相应的 C 的第 id 个分块。
 实现：见[附件][2]。
 测试：调整数据规模为 1000 * 2000 * 800，既耗时不长，又可体现并行和串行的明显差异
 ![](/img/Snipaste_2020-04-06_17-51-07.png)
-#### 优化
+#### 利用 MPI_Send 和 MPI_Recv 代替 MPI_Bcast
+>参考[教程][3]
 
+实现：见[附件][4]。
+测试：增大数据规模，见下图
+![](/img/Snipaste_2020-04-06_20-01-33.png)
+可以看到，数据量大时用重写的方法性能更好。
+但就性能上说，实验结果与[教程][3]中的结论相反，不知道为什么。
 
-
-
+另外，性能分析工具莫名其妙报错了，我只能通过 clock() 来手动计算时间。
+![](/img/Snipaste_2020-04-07_01-05-23.png)
 
 
 
@@ -26,3 +37,6 @@
 
 [1]:/files/chuan1.c
 [2]:/files/Scatter-Gather.c
+[3]:https://mpitutorial.com/tutorials/mpi-broadcast-and-collective-communication/
+[4]:/files/SendRecv.c
+[5]:/files/ini-sendRecv.c
---
+++ files/ini-sendRecv.c
@@ -0,0 +1,71 @@
+#include <string.h>
+#include <sys/time.h>
+#include <time.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <mpi.h>
+#define N   1000
+#define M		1000
+MPI_Status status;
+
+double a[N][M],b[M][N],c[N][N];
+
+int main(int argc, char **argv)
+{
+  int numtasks,taskid,numworkers,source,dest,rows,offset,i,j,k;
+
+  struct timeval start, stop;
+  time_t tt0=clock();
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &taskid);
+  MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+  numworkers = numtasks-1;
+  if (taskid == 0) {
+    for (i=0; i<N; i++) {
+      for (j=0; j<M; j++) {
+        a[i][j]= 1.0;
+        b[i][j]= 2.0;
+      }
+    }
+    rows = N/numworkers;
+    offset = 0;
+
+    for (dest=1; dest<=numworkers; dest++)
+    {
+      MPI_Send(&offset, 1, MPI_INT, dest, 1, MPI_COMM_WORLD);
+      MPI_Send(&rows, 1, MPI_INT, dest, 1, MPI_COMM_WORLD);
+      MPI_Send(&a[offset][0], rows*M, MPI_DOUBLE,dest,1, MPI_COMM_WORLD);
+      MPI_Send(&b, M*N, MPI_DOUBLE, dest, 1, MPI_COMM_WORLD);
+      offset = offset + rows;
+    }
+    for (i=1; i<=numworkers; i++)
+    {
+      source = i;
+      MPI_Recv(&offset, 1, MPI_INT, source, 2, MPI_COMM_WORLD, &status);
+      MPI_Recv(&rows, 1, MPI_INT, source, 2, MPI_COMM_WORLD, &status);
+      MPI_Recv(&c[offset][0], rows*M, MPI_DOUBLE, source, 2, MPI_COMM_WORLD, &status);
+    }
+  }
+  if (taskid > 0) {
+    source = 0;
+    MPI_Recv(&offset, 1, MPI_INT, source, 1, MPI_COMM_WORLD, &status);
+    MPI_Recv(&rows, 1, MPI_INT, source, 1, MPI_COMM_WORLD, &status);
+    MPI_Recv(&a, rows*M, MPI_DOUBLE, source, 1, MPI_COMM_WORLD, &status);
+    MPI_Recv(&b, M*N, MPI_DOUBLE, source, 1, MPI_COMM_WORLD, &status);
+    for (k=0; k<N; k++)
+      for (i=0; i<rows; i++) {
+        c[i][k] = 0.0;
+        for (j=0; j<M; j++)
+          c[i][k] = c[i][k] + a[i][j] * b[j][k];
+      }
+    MPI_Send(&offset, 1, MPI_INT, 0, 2, MPI_COMM_WORLD);
+    MPI_Send(&rows, 1, MPI_INT, 0, 2, MPI_COMM_WORLD);
+    MPI_Send(&c, rows*M, MPI_DOUBLE, 0, 2, MPI_COMM_WORLD);
+  }
+
+  MPI_Finalize();
+time_t tt1=clock();
+ printf("rank %d        CPU 时间:%.6lf seconds\n", taskid, (double)(tt1-tt0)/CLOCKS_PER_SEC);
+}
